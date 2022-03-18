9fa43866605dc6f2af1a4e243a0ae950649a6104
MacSoares <macario.junior@gmail.com>
2022-03-18 17:25:49 -0300

    Working connectors and tests

---
+++ data-pyetl/data_pyetl/connectors.py
@@ -0,0 +1,58 @@
+from sqlalchemy.engine import create_engine
+import urllib
+
+
+class DB_Connector:
+
+    def __init__(self, data_source_engine, credentials):
+        
+        if type(data_source_engine) is not str:
+            raise Exception("Data source engine must be a string")
+        else:
+            self.data_source_engine = data_source_engine
+        
+        if type(credentials) is not dict:
+            raise Exception("The variable 'redentials' must be a dict of the data source credentials")
+        else:
+            self.credentials = credentials
+
+
+    def create_data_source_connection(self):
+        if self.data_source_engine == "SQL":
+            params = urllib.parse.quote_plus(
+                "DRIVER={ODBC Driver 17 for SQL Server};"
+                + "SERVER="
+                + self.credentials["host"]
+                + ";DATABASE="
+                + self.credentials["db"]
+                + ";UID="
+                + self.credentials["username"]
+                + ";PWD="
+                + self.credentials["pwd"]
+            )
+            engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+            
+        elif self.data_source_engine == "Oracle":
+            engine = create_engine(
+                f"oracle+cx_oracle://{self.credentials['username']}:{self.credentials['pwd']}@{self.credentials['host']}:{self.credentials['port']}/?service_name={self.credentials['service_name']}"
+            )
+
+        elif self.data_source_engine == "MySQL":
+            engine = create_engine(
+                f"mysql+pymysql://{self.credentials['username']}:{self.credentials['pwd']}@{self.credentials['host']}/{self.credentials['db']}",
+                pool_recycle=3600,
+            )
+
+        elif self.data_source_engine == "Postgres":
+            engine = create_engine(
+                f"postgresql://{self.credentials['username']}:{self.credentials['pwd']}@{self.credentials['host']}:{self.credentials['port']}/{self.credentials['db']}",
+                pool_recycle=3600,
+            )
+
+        elif self.data_source_engine == "Firebird":
+            engine = create_engine(
+                f"firebird://{self.credentials['username']}:{self.credentials['pwd']}@{self.credentials['host']}:{self.credentials['port']}/{self.credentials['db']}",
+                pool_recycle=3600,
+            )
+
+        return engine
--- data-pyetl/tests/test_data_pyetl.py
+++ data-pyetl/tests/test_data_pyetl.py
@@ -1,5 +1,107 @@
-from data_pyetl import __version__
+import pytest
+import unittest
+from data_pyetl.connectors import DB_Connector
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+class TestConnectors(unittest.TestCase):
+
+    def test_error_on_init_data_source(self):
+        ds_con = ["Not a string"]
+        credentials = {
+            "host":"host", 
+            "db":"db",
+            "username":"username",
+            "pwd":"pwd"
+        }
+
+        with pytest.raises(Exception, match=r"Data source engine must be a string"):
+            connector = DB_Connector(ds_con, credentials)
+    
+    def test_error_on_init_credentials(self):
+        ds_con = "SQL"
+        credentials = ["Not a dict"]
+
+        with pytest.raises(Exception, match=r"The variable 'redentials' must be a dict of the data source credentials"):
+            connector = DB_Connector(ds_con, credentials)
+ 
+    def test_instance_created(self):
+        ds_con = "SQL"
+        credentials = {
+            "host":"host", 
+            "db":"db",
+            "username":"username",
+            "pwd":"pwd"
+        }
+        connector = DB_Connector(ds_con, credentials)
+
+        self.assertIsInstance(connector, DB_Connector)
+    
+    def test_create_sqlserver_connector(self):
+        ds_con = "SQL"
+        credentials = {
+            "host":"host", 
+            "db":"db",
+            "username":"username",
+            "pwd":"pwd"
+        }
+        connector = DB_Connector(ds_con, credentials)
+        engine = connector.create_data_source_connection()
+
+        self.assertEqual(engine.name, "mssql")
+
+    def test_create_mysql_connector(self):
+        ds_con = "MySQL"
+        credentials = {
+            "host":"host", 
+            "db":"db",
+            "username":"username",
+            "pwd":"pwd"
+        }
+        connector = DB_Connector(ds_con, credentials)
+        engine = connector.create_data_source_connection()
+
+        self.assertEqual(engine.name, "mysql")
+
+    def test_create_postgres_connector(self):
+        ds_con = "Postgres"
+        credentials = {
+            "host":"host", 
+            "db":"db",
+            "username":"username",
+            "pwd":"pwd",
+            "port":"1234"
+        }
+        connector = DB_Connector(ds_con, credentials)
+        engine = connector.create_data_source_connection()
+
+        self.assertEqual(engine.name, "postgresql")
+
+    def test_create_firebird_connector(self):
+        ds_con = "Firebird"
+        credentials = {
+            "host":"host", 
+            "db":"db",
+            "username":"username",
+            "pwd":"pwd",
+            "port":"1234"
+        }
+        connector = DB_Connector(ds_con, credentials)
+        engine = connector.create_data_source_connection()
+
+        self.assertEqual(engine.name, "firebird")
+
+    def test_create_oracle_connector(self):
+        ds_con = "Oracle"
+        credentials = {
+            "host":"host", 
+            "db":"db",
+            "username":"username",
+            "pwd":"pwd",
+            "port":"1234",
+            "service_name":"name1"
+        }
+        connector = DB_Connector(ds_con, credentials)
+        engine = connector.create_data_source_connection()
+
+        self.assertEqual(engine.name, "oracle")
+
