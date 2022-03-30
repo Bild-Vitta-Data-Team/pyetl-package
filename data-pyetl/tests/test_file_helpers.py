4ac5ff35eddb4693adfa9b52fa4b3d49a6190648
MacSoares <macario.junior@gmail.com>
2022-03-30 16:23:16 -0300

    Creating tests for file helper

---
+++ data-pyetl/tests/test_file_helpers.py
@@ -0,0 +1,148 @@
+import pytest
+import unittest
+import requests_mock
+import pandas as pd
+from pandas import util
+from unittest.mock import MagicMock, Mock, patch
+from data_pyetl.file_helper import FileHelper
+from data_pyetl.connectors import DB_Connector
+
+
+class TestFileHelpers(unittest.TestCase):
+
+    def setUp(self):
+        test_data = {"Unnamed: 0": {"0": 1, "1": 2, "2": 3, "3": 4}, "a": {"0": "q", "1": "t", "2": "o", "3": "d"}, "b": {
+            "0": "w", "1": "y", "2": "p", "3": "f"}, "c": {"0": "e", "1": "u", "2": "a", "3": "g"}, "d": {"0": "r", "1": "i", "2": "s", "3": "h"}}
+        self.df_test = pd.DataFrame(test_data)
+        self.filetype = "xlsx"
+        self.filepath = f"/test.{self.filetype}"
+        self.helper = FileHelper(self.filepath, self.filetype)
+
+    def test_error_on_init_filepath(self):
+        filepath = ["./test.xlsx"]
+        filetype = "xlsx"
+
+        with pytest.raises(Exception, match=r"Filepath must be a string"):
+            helper = FileHelper(filepath, filetype)
+
+    def test_error_on_init_filetype(self):
+        filepath = "./test.xlsx"
+        filetype = ["xlsx"]
+
+        with pytest.raises(Exception, match=r"Filetype must be a string"):
+            helper = FileHelper(filepath, filetype)
+
+    def test_init_success(self):
+        filepath = "./test.xlsx"
+        filetype = "xlsx"
+        helper = FileHelper(filepath, filetype)
+
+        self.assertIsInstance(helper, FileHelper)
+
+    def test_get_xsl_data_dataframe(self):
+        self.helper.filetype = "xls"
+        self.helper.filepath = "tests/test.xls"
+        df_data = self.helper.read_xls_to_dataframe()
+
+        self.assertIsInstance(df_data, pd.DataFrame)
+
+    @patch('pandas.read_excel', **{'return_value.raiseError.side_effect': Exception()})
+    def test_get_xsl_data_dataframe_error(self, excel_mock: Mock):
+        excel_mock.side_effect = Exception("Failed to mount dataframe")
+        self.helper.filetype = "xls"
+        self.helper.filepath = "tests/test.xls"
+        df_data = self.helper.read_xls_to_dataframe()
+
+        excel_mock.assert_called()
+        self.assertIsInstance(df_data, Exception)
+        self.assertEqual(df_data, excel_mock.side_effect)
+
+    def test_get_xslx_data_dataframe(self):
+        self.helper.filetype = "xlsx"
+        self.helper.filepath = "tests/test.xlsx"
+        df_data = self.helper.read_xls_to_dataframe()
+
+        self.assertIsInstance(df_data, pd.DataFrame)
+
+    @patch('pandas.read_excel', **{'return_value.raiseError.side_effect': Exception()})
+    def test_get_xslx_data_dataframe_error(self, excel_mock: Mock):
+        excel_mock.side_effect = Exception("Failed to mount dataframe")
+        self.helper.filetype = "xlsx"
+        self.helper.filepath = "tests/test.xlsx"
+        df_data = self.helper.read_xls_to_dataframe()
+
+        excel_mock.assert_called()
+        self.assertIsInstance(df_data, Exception)
+        self.assertEqual(df_data, excel_mock.side_effect)
+
+    def test_get_ods_data_dataframe(self):
+        self.helper.filetype = "ods"
+        self.helper.filepath = "tests/test.ods"
+        df_data = self.helper.read_xls_to_dataframe()
+
+        self.assertIsInstance(df_data, pd.DataFrame)
+
+    @patch('pandas.read_excel', **{'return_value.raiseError.side_effect': Exception()})
+    def test_get_ods_data_dataframe_error(self, excel_mock: Mock):
+        excel_mock.side_effect = Exception("Failed to mount dataframe")
+        self.helper.filetype = "ods"
+        self.helper.filepath = "tests/test.ods"
+        df_data = self.helper.read_xls_to_dataframe()
+
+        excel_mock.assert_called()
+        self.assertIsInstance(df_data, Exception)
+        self.assertEqual(df_data, excel_mock.side_effect)
+
+    def test_get_csv_data_dataframe(self):
+        self.helper.filetype = "csv"
+        self.helper.filepath = "tests/test.csv"
+        df_data = self.helper.read_csv_to_dataframe()
+
+        self.assertIsInstance(df_data, pd.DataFrame)
+
+    @patch('pandas.read_csv', **{'return_value.raiseError.side_effect': Exception()})
+    def test_get_csv_data_dataframe_error(self, csv_mock: Mock):
+        csv_mock.side_effect = Exception("Failed to mount dataframe")
+        self.helper.filetype = "csv"
+        self.helper.filepath = "tests/test.csv"
+        df_data = self.helper.read_csv_to_dataframe()
+
+        csv_mock.assert_called()
+        self.assertIsInstance(df_data, Exception)
+        self.assertEqual(df_data, csv_mock.side_effect)
+
+    def test_get_json_data_dataframe(self):
+        self.helper.filetype = "json"
+        self.helper.filepath = "tests/test.json"
+        df_data = self.helper.read_json_to_dataframe()
+
+        self.assertIsInstance(df_data, pd.DataFrame)
+
+    @patch('pandas.read_json', **{'return_value.raiseError.side_effect': Exception()})
+    def test_get_json_data_dataframe_error(self, json_mock: Mock):
+        json_mock.side_effect = Exception("Failed to mount dataframe")
+        self.helper.filetype = "json"
+        self.helper.filepath = "tests/test.json"
+        df_data = self.helper.read_json_to_dataframe()
+
+        json_mock.assert_called()
+        self.assertIsInstance(df_data, Exception)
+        self.assertEqual(df_data, json_mock.side_effect)
+
+    def test_get_xml_data_dataframe(self):
+        self.helper.filetype = "xml"
+        self.helper.filepath = "tests/test.xml"
+        df_data = self.helper.read_xml_to_dataframe()
+
+        self.assertIsInstance(df_data, pd.DataFrame)
+
+    @patch('pandas.read_xml', **{'return_value.raiseError.side_effect': Exception()})
+    def test_get_xml_data_dataframe_error(self, xml_mock: Mock):
+        xml_mock.side_effect = Exception("Failed to mount dataframe")
+        self.helper.filetype = "xml"
+        self.helper.filepath = "tests/test.xml"
+        df_data = self.helper.read_xml_to_dataframe()
+
+        xml_mock.assert_called()
+        self.assertIsInstance(df_data, Exception)
+        self.assertEqual(df_data, xml_mock.side_effect)
